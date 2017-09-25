3805de17e4b095a39237edfa8b951b1e6cafd5be
Mike Croall <mikebcroall@gmail.com>
2017-09-25 23:10:55 +0100

    Image to single colour merging // Cleaner file names

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Tested on Windows 10 using Python 3.6.0, with Numpy 1.13.1, and OpenCV 3.3.0
 
-import sys  # Shouldn't fail, who runs python scripts on a non-existent system?
+import sys, os  # Shouldn't fail, who runs python scripts on a non-existent system?
 
 try:
     import numpy as np
@@ -19,12 +19,16 @@ except ImportError:
     sys.exit(1)
 
 
+def strip_extension(file_name):
+    base = os.path.basename(file_name)
+    return os.path.splitext(base)[0]
+
+
 def print_pre_video_commands():
     print("\nCommands (followed by enter to confirm)")
     print("\tv - load video file")
     print("\ti - load previously outputted image file")
-    print("\tx - cancel")
-    print("\n")
+    print("\tx - quit\n")
 
 
 def print_video_processed_commands():
@@ -35,6 +39,10 @@ def print_video_processed_commands():
     print("\nPress your chosen key on the image window\n")
 
 
+def load_image(file):
+    return cv2.imread("output/{}".format(file)), strip_extension(file)
+
+
 def load_video(file):
     vc = cv2.VideoCapture("video/{}".format(file))
 
@@ -43,7 +51,7 @@ def load_video(file):
         return None
     else:
         print("Video opened")
-        output_file = "output/{}.png".format(file)
+        output_file = "output/{}.png".format(strip_extension(file))
         keep_going, frame = vc.read()
         frame_count = 1
         rows, columns, channels = frame.shape
@@ -73,9 +81,9 @@ def load_video(file):
             r /= total_pixels_in_frame
             g /= total_pixels_in_frame
             b /= total_pixels_in_frame
-            r = round(r)
-            g = round(g)
-            b = round(b)
+            r = int(round(r))
+            g = int(round(g))
+            b = int(round(b))
 
             output_image[:, frame_count - 1] = (b, g, r)  # BGR, not RGB, because OpenCV
 
@@ -94,40 +102,85 @@ def load_video(file):
         print("Saving output to {}".format(output_file))
         cv2.imwrite(output_file, output_image)
 
-        return output_image
+        return output_image, strip_extension(file)
 
 
-working_image = None
-print_pre_video_commands()
-invalid_answer = True
-while invalid_answer:
-    invalid_answer = False
-    option = input("Option: ").lower().strip()
-    if option == 'v':
-        working_image = load_video(input("Video file to use: "))
-    elif option == 'i':
-        print("Not yet implemented")  # TODO implement previous image loading
-    elif option == 'x':
-        sys.exit(0)
+def merge_image(img, file_name):
+    print("Assuming image is output of this program, and therefore only the first row of pixels will be merged")
+    r, g, b = 0, 0, 0
+    rows, columns, channels = img.shape
+
+    output_image = np.zeros((400, 400, 3), np.uint8)
+
+    if channels != 3:
+        print("Image doesn't have exactly 3 colour channels, unexpected format, exiting...")
+        sys.exit(1)
+
+    for x in range(columns):
+        r += img[0, x, 2]
+        g += img[0, x, 1]
+        b += img[0, x, 0]
+
+    r /= columns
+    g /= columns
+    b /= columns
+    r = int(round(r))
+    g = int(round(g))
+    b = int(round(b))
+
+    output_image[:, :] = (b, g, r)
+
+    output_file = "merge/{} R{} G{} B{}.png".format(strip_extension(file_name), r, g, b)
+
+    print("Saving output to {}".format(output_file))
+    cv2.imwrite(output_file, output_image)
+
+    cv2.imshow(output_file, output_image)
+
+
+def main():
+    working_title = "no-file"
+    working_image = None
+    print_pre_video_commands()
+    invalid_answer = True
+    while invalid_answer:
+        invalid_answer = False
+        option = input("Option: ").lower().strip()
+        if option == 'v':
+            working_image, working_title = load_video(input("Video file to use: video/"))
+        elif option == 'i':
+            working_image, working_title = load_image(input("Image file to use: output/"))
+        elif option == 'x':
+            sys.exit(0)
+        else:
+            invalid_answer = True
+            print_pre_video_commands()
+
+    if working_image is None:
+        print("Working image was unexpectedly null")
+        sys.exit(1)
     else:
-        invalid_answer = True
-        print_pre_video_commands()
+        keep_alive = True
+        printed_commands = False
+        while keep_alive:
+            cv2.imshow("output preview, see console for commands", working_image)
 
-if working_image is not None:
-    keep_alive = True
-    printed_commands = False
-    while keep_alive:
-        cv2.imshow("output preview, see console for commands", working_image)
+            if not printed_commands:
+                print_video_processed_commands()
+                printed_commands = True
 
-        if not printed_commands:
-            print_video_processed_commands()
-            printed_commands = True
+            key = cv2.waitKey(40) & 0xFF  # cv2.waitKey(0)
 
-        key = cv2.waitKey(40) & 0xFF  # cv2.waitKey(0)
+            if key == ord('x'):
+                keep_alive = False
+            elif key == ord('m'):
+                merge_image(working_image, working_title)
+                printed_commands = False
+            elif key == ord('s'):
+                print(
+                    "Not yet implemented, for now use just the video/image section you want merged as input")  # TODO implement selection for merge
+                # Automatically ignores invalid answers
 
-        if key == ord('x'):
-            keep_alive = False
-        elif key == ord('m'):
-            print("Not yet implemented")  # TODO implement merge all
-        elif key == ord('s'):
-            print("Not yet implemented")  # TODO implement selection for merge
+
+if __name__ == "__main__":
+    main()
