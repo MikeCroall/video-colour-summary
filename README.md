74aac2704e52f76fb9deb01ae43b5f9e15e2f901
Mike Croall <mikebcroall@gmail.com>
2017-09-25 01:17:28 +0100

    Refactor // Menus // TODOs

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # video colour summary
 
 ## Summarise a video into a single image
+This script does not take shortcuts. Every pixel, of every frame, is taken into account.
 
 Tested with
 - `Windows 10`
 - `Python 3.6.0`
 - `Numpy 1.13.1`
 - `OpenCV 3.3.0`
+- `Only the provided Big Buck Bunny snippet as mp4`
 
 ### Getting Started
 1. Place your video file in the `video` folder
-2. In `main.py`, change the value of the `file_name` variable, including your file's extension
-3. Run `main.py`, and wait - it isn't very fast!
-4. Once complete, the script will save your image into the `output` folder, and show you a preview of it
-5. Press any key to close the preview window, and exit the script
+2. Run `main.py`, and follow the printed menu system in the console
+3. While processing, be patient - **it isn't very fast!** (It only looks at *every single pixel* of *every single frame*)
+4. Any saved images will be in the `output` folder, having been announced in the console at the time of saving
+5. Press `x`, as the menu states, to exit the script cleanly
 
 ### Thanks
 Big Buck Bunny Test Video Courtesy of https://peach.blender.org/
--- main.py
+++ main.py
@@ -1,72 +1,131 @@
 # Tested on Windows 10 using Python 3.6.0, with Numpy 1.13.1, and OpenCV 3.3.0
 
-import sys
-import numpy as np
+import sys  # Shouldn't fail, who runs python scripts on a non-existent system?
+
 try:
-    import cv2
-    print("Using OpenCV version " + cv2.__version__)
+    import numpy as np
+
+    print("Using Numpy version {}".format(np.__version__))
 except ImportError:
-    print("Could not import OpenCV")
+    print("Could not import Numpy, is it installed? Exiting...")
     sys.exit(1)
 
-file_name = "big_buck_bunny.mp4"
+try:
+    import cv2
 
-output_file = "output/{}.jpg".format(file_name)
+    print("Using OpenCV version {}".format(cv2.__version__))
+except ImportError:
+    print("Could not import OpenCV, is it installed? Exiting...")
+    sys.exit(1)
 
-vc = cv2.VideoCapture("video/{}".format(file_name))
 
-if vc.isOpened():
-    print("Video opened")
+def print_pre_video_commands():
+    print("\nCommands")
+    print("\tv - load video file")
+    print("\ti - load previously outputted image file")
+    print("\tx - cancel")
+    print("\n")
 
-    keep_going, frame = vc.read()
-    frame_count = 0
-    rows, columns, channels = frame.shape
-    total_pixels_in_frame = rows * columns
-    total_frames = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    if channels != 3:
-        print("Video doesn't have exactly 3 colour channels, unexpected format")
-        sys.exit(1)
+def print_video_processed_commands():
+    print("\nCommands")
+    print("\tm - merge all to single colour")
+    print("\ts - select area to merge to single colour")
+    print("\tx - quit")
+    print("\n")
 
-    print("{} rows, {} columns, {} colour channels".format(rows, columns, channels))
-    print("Reported frames: {}".format(total_frames))
 
-    output_image = np.zeros((int(total_frames / 10), total_frames, 3), np.uint8)
+def load_video(file):
+    vc = cv2.VideoCapture("video/{}".format(file))
 
-    while keep_going:
-        r, g, b = 0, 0, 0
-        for y in range(rows):
-            for x in range(columns):
-                r += frame[y, x, 2]
-                g += frame[y, x, 1]
-                b += frame[y, x, 0]
-
-        r /= total_pixels_in_frame
-        g /= total_pixels_in_frame
-        b /= total_pixels_in_frame
-        r = round(r)
-        g = round(g)
-        b = round(b)
-
-        output_image[:, frame_count] = (b, g, r)  # BGR, not RGB, because OpenCV
-
+    if not vc.isOpened():
+        print("Video could not be opened")
+        return None
+    else:
+        print("Video opened")
+        output_file = "output/{}.jpg".format(file)  # TODO output to PNG to avoid compression?
         keep_going, frame = vc.read()
-        frame_count += 1
-        if frame_count % 10 == 0:
+        frame_count = 0
+        rows, columns, channels = frame.shape
+        total_pixels_in_frame = rows * columns
+        total_frames = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        if channels != 3:
+            print("Video doesn't have exactly 3 colour channels, unexpected format, exiting...")
+            sys.exit(1)
+
+        print("{} rows, {} columns, {} colour channels".format(rows, columns, channels))
+        print("Reported frames: {}".format(total_frames))
+
+        output_image = np.zeros((int(total_frames / 10), total_frames, 3), np.uint8)
+        print("\rProgress: {}/{}".format(frame_count, total_frames), end="")
+
+        while keep_going:
+            r, g, b = 0, 0, 0
+            for y in range(rows):
+                for x in range(columns):
+                    r += frame[y, x, 2]
+                    g += frame[y, x, 1]
+                    b += frame[y, x, 0]
+
+            r /= total_pixels_in_frame
+            g /= total_pixels_in_frame
+            b /= total_pixels_in_frame
+            r = round(r)
+            g = round(g)
+            b = round(b)
+
+            output_image[:, frame_count] = (b, g, r)  # BGR, not RGB, because OpenCV
+
+            keep_going, frame = vc.read()
+            frame_count += 1
             print("\rProgress: {}/{}".format(frame_count, total_frames), end="")
 
-    vc.release()
+        vc.release()
+        print()  # To stop the Progress one line overwriting from being overwritten
 
-    # frame_count was incremented regardless of keep_going, so check frame_count - 1
-    if frame_count - 1 < total_frames:
-        print("\nStopped at frame {}, cropping...".format(frame_count - 1))
-        output_image = output_image[:, :frame_count-1]
+        # frame_count was incremented regardless of keep_going, so check frame_count - 1
+        if frame_count - 1 < total_frames:
+            print("Stopped at frame {}, cropping...".format(frame_count - 1))
+            output_image = output_image[:, :frame_count - 1]
 
-    print("Saving output to {}".format(output_file))
-    cv2.imwrite(output_file, output_image)
+        print("Saving output to {}".format(output_file))
+        cv2.imwrite(output_file, output_image)
 
-    cv2.imshow("output preview, press key to close", output_image)
-    cv2.waitKey(0)
+        return output_image
 
-else:
-    print("Video could not be opened")
+
+working_image = None
+print_pre_video_commands()
+invalid_answer = True
+while invalid_answer:
+    invalid_answer = False
+    option = input("Option: ").lower().strip()
+    if option == 'v':
+        working_image = load_video(input("Video file to use: "))
+    elif option == 'i':
+        print("Not yet implemented")  # TODO implement previous image loading
+    elif option == 'x':
+        sys.exit(0)
+    else:
+        invalid_answer = True
+        print_pre_video_commands()
+
+if working_image is not None:
+    keep_alive = True
+    printed_commands = False
+    while keep_alive:
+        cv2.imshow("output preview, see console for commands", working_image)
+
+        if not printed_commands:
+            print_video_processed_commands()
+            printed_commands = True
+
+        key = cv2.waitKey(40) & 0xFF  # cv2.waitKey(0)
+
+        if key == ord('x'):
+            keep_alive = False
+        elif key == ord('m'):
+            print("Not yet implemented")  # TODO implement merge all
+        elif key == ord('s'):
+            print("Not yet implemented")  # TODO implement selection for merge
